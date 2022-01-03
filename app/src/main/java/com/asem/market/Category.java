26d4f67d0529169da2d9fd6b926369b3ee0522f8
MohammedAliMohammedAlobeed <96793636+MohammedAliMohammedAlobeed@users.noreply.github.com>
2022-01-03 16:33:07 +0300

    Move the getters from sub class to parent class

--- app/src/main/java/com/asem/market/Category.java
+++ app/src/main/java/com/asem/market/Category.java
@@ -7,12 +7,37 @@ public class Category {
     protected int quantity;
     protected int production_Date;
     protected int expiration_Date;
-
+    
+    public Category(){}
     public Category(String name, float price, int quantity, int production_Date, int expiration_Date) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.production_Date = production_Date;
         this.expiration_Date = expiration_Date;
+    }
+     
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getProduction_Date() {
+        return production_Date;
+    }
+
+    public int getExpiration_Date() {
+        return expiration_Date;
+    }
+    public int returnDate()
+    {
+    return expiration_Date-production_Date;
     }
 }
