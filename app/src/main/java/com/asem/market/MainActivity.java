ab79652f3b5a6d13016a368edd1ab87ddcb8ade1
Asem Al-haidary <asemalhaidary.2001@gmail.com>
2022-01-04 18:10:03 +0300

    Rearrange the code and fix some problems

--- app/src/main/java/com/asem/market/MainActivity.java
+++ app/src/main/java/com/asem/market/MainActivity.java
@@ -1,20 +1,21 @@
 package com.asem.market;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.asem.market.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,13 +41,23 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
-                .build();
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_all, R.id.nav_preserves, R.id.nav_drinks).setOpenableLayout(drawer).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+//        ArrayList<String> wordsList = new ArrayList<String>();
+//
+//        wordsList.add("one");
+//        wordsList.add("one");
+//        wordsList.add("one");
+//        wordsList.add("one");
+//        wordsList.add("one");
+//        wordsList.add("one");
+//
+//        ArrayAdapter<String> wordsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wordsList);
+//        ListView productList = (ListView) findViewById(R.id.products_list);
+//        productList.setAdapter(wordsAdapter);
     }
 
     @Override
