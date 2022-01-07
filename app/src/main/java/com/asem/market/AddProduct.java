539ecac713b42bad9205940bdb18eb4931a49b20
Asem Al-haidary <asemalhaidary.2001@gmail.com>
2022-01-07 11:15:12 +0300

    Create Add product Activity

---
+++ app/src/main/java/com/asem/market/AddProduct.java
@@ -0,0 +1,14 @@
+package com.asem.market;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
